be09822695f1c806579d0daf4a64142c7e4721f5
diegocantelli <diegocantelli19@gmail.com>
2023-09-05 11:35:32 -0300

    Applying the bind between the form and the formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'form-array-tutorial';
+  customerInfo!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.customerInfo = this.formBuilder.group({
+      firstName: [],
+      lastName: [],
+      username: [],
+      email: [],
+      products: this.formBuilder.array([])
+    })
+  }
+
+  addProduct() {
+    let products = this.customerInfo.get('products') as FormArray;
+    products.push(this.formBuilder.group({
+      name : [],
+      description : []
+    }));
+  }
 }
